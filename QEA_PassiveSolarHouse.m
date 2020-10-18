6389689365fa6effc3c51284ae3345c91344771c
Walter <60273122+notwaltervilla@users.noreply.github.com>
2020-10-18 14:16:38 -0400

    Update QEA_PassiveSolarHouse.m
    Added annotations for upper and lower temperature values reached during equilibrium. Extended plot for a span of 2 weeks vs. original 10 days.

--- QEA_PassiveSolarHouse.m
+++ QEA_PassiveSolarHouse.m
@@ -39,30 +39,49 @@ R_roof = (1/(h_indoor * A_roof)) + (L_roof/(k_roof * A_roof)) + (1/(h_outdoor * 
 R_window = (1/(h_indoor * A_window)) + (1/(h_window * A_window)) + (1/(h_outdoor * A_window));
 
 %Calculating total resistance of system (K/W)%
-R_total = ((1/R_floor) + (1/R_wall) + (1/R_roof) + (1/R_window))^(-1); 
+R_parallel = ((1/R_floor) + (1/R_wall) + (1/R_roof) + (1/R_window))^(-1); 
+R1 = 1/((h_indoor)*(A_floor));
+R_total = R_parallel + R1;
 
 % Plotting and Solving ODE %
 %Q_in = -361*cos((3.14*t)/(12*3600)) + 224*cos((3.14*t)/(6*3600)) + 210; % Temperature modeled as a function of time %
-tspan = [0 86400*10];
+
+tspan = 0:86400*14;
 [t, T] = ode45(@(t,T) (((T - T_out)/ R_total) - (-361*cos((3.14*t)/(12*3600)) + 224*cos((3.14*t)/(6*3600)) + 210)) / (-C_HeatStorage) , tspan, -3);
 plot(t/86400, T, '-');
-title('Temperature of Air in Room vs. Time (Span over 10 days)');
+title('Temperature of Air in Room vs. Time (Span over 2 weeks)');
 xlabel('Time (days)');
-ylabel('Temperature of Air');
+ylabel('Temperature of Air (^{o}C');
+
+% Plot when reached equillibirum after 24 hours %
+t_adjust = t(86400*7:86400*8);
+T_adjust = T(86400*7:86400*8);
+t_days = t_adjust/86400;
+
+% Finding Min and Max%
+[maxtemp,maxtempindex] = max(T_adjust);
+[mintemp,mintempindex] = min(T_adjust);
+
+% Plot max and min first(idk why its buggy)
+plot(t_days([maxtempindex]), maxtemp,"or");
+hold on
+plot(t_days([mintempindex]), mintemp,"or");
+% Label points with max and min values
+text(t_days([maxtempindex]), maxtemp,'      29.1642');
+text(t_days([mintempindex]), mintemp,'      18.3633');
+
+% Plot 1 day @ Equillibrium
+plot(t_adjust/86400,T_adjust);
+title('Temperature of Air in Room vs. Time (24 hr Equillibrium)');
+xlabel('Time (hours )');
+ylabel('Temperature of Air (^{o}C');
+hold off
 
 % Plotting First 24 hours%
 tspan = [0 86400];
 [t, T] = ode45(@(t,T) (((T - T_out)/ R_total) - (-361*cos((3.14*t)/(12*3600)) + 224*cos((3.14*t)/(6*3600)) + 210)) / (-C_HeatStorage) , tspan, -3);
 plot(t/3600, T, '-');
-title('Temperature of Air in Room vs. Time (Span over 24 hours)');
+title('Temperature of Air in Room vs. Time (First 24 hours)');
 xlabel('Time (24 hours)');
 ylabel('Temperature of Air');
 
-% Plotting 24 hours once reached equillibrium%
-tspan = [0 86400*10];
-[t, T] = ode45(@(t,T) (((T - T_out)/ R_total) - (-361*cos((3.14*t)/(12*3600)) + 224*cos((3.14*t)/(6*3600)) + 210)) / (-C_HeatStorage) , tspan, -3);
-plot(t/86400, smooth(T), '-');
-title('Temperature of Air in Room vs. Time (24 hours reached equillibrium)');
-xlabel('Time (24 hours)');
-ylabel('Temperature of Air');
-xlim([5,6]);
